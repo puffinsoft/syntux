276b778c46172ca10a8e210f162a0eeaca527553
Grant <65585002+ColonelParrot@users.noreply.github.com>
2025-12-18 05:02:42 -0500

    export additional types

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@ import { createGeneratedPage } from './GeneratedPage';
 import { GeneratedContent } from './GeneratedContent';
 import { Renderer } from './Renderer';
 
+export { GeneratedContentProps } from './GeneratedContent';
 export { GeneratedPageProps } from './GeneratedPage';
+export {SchemaNode} from './Renderer';
 
 export interface SyntuxFactoryConfig {
     model: LanguageModel;
 }
 
 
-export const createSyntuxFactory = (config: SyntuxFactoryConfig)=>{
+export const createSyntuxFactory = (config: SyntuxFactoryConfig) => {
     return {
         GeneratedPage: createGeneratedPage(config.model),
         GeneratedContent,

4d3a5971977b4e178c0942058cd2d25882a30089
Grant <65585002+ColonelParrot@users.noreply.github.com>
2025-12-27 11:56:24 -0500

    Rewrite renderer in accordance to new spec

--- src/Renderer.tsx
+++ src/Renderer.tsx
@@ -1,84 +1,78 @@
-import { ComponentType } from 'react'
+import { ComponentType, Fragment } from 'react'
+import { ChildrenMap, ComponentMap, SchemaNode } from './types';
 
 const resolvePath = (obj: any, path: string) => {
-    if(path === '$') return obj;
+    if (path === '$') return obj;
     return path.split('.').reduce((acc, curr) => acc?.[curr], obj)
 }
 
 const get = (global: any, local: any, path: string) => {
-    if(path.startsWith("$item.")){
+    if (path.startsWith("$item.")) {
         path = path.slice(6)
         return resolvePath(local, path);
     } else {
-        if(path === "$item") return local;
+        if (path === "$item") return local;
         return resolvePath(global, path);
     }
 }
 
 const resolveProps = (global: any, local: any, props: any) => {
-    if(!props) return;
-    if("$bind" in props) return get(global, local, props.$bind); // $bind may be falsy value
+    if (!props) return;
+    if ("$bind" in props) return get(global, local, props.$bind); // $bind may be falsy value
     Object.keys(props).forEach((key) => {
         const val = props[key];
-        if(typeof val === "object"){
+        if (typeof val === "object") {
             props[key] = resolveProps(global, local, val);
         }
     })
     return props;
 }
 
-export interface SchemaNode {
-    type?: string;
-    props?: Record<string, any>;
-    children?: (SchemaNode | string)[];
-    source?: string;
-    template?: SchemaNode;
-    $bind?: string;
-}
-
 export interface RendererProps {
-    schema: SchemaNode | string; // string occurs recursively
+    id: string;
+    componentMap: ComponentMap;
+    childrenMap: ChildrenMap;
+    allowedComponents: Record<string, ComponentType<any> | string>;
     global: any;
-    local?: any;
-    components: Record<string, ComponentType<any> | string>;
+    local: any;
 }
 
-export function Renderer({
-    schema, global, local, components
-}: RendererProps) {
-    if(typeof schema === "string") return <>{schema}</>;
-    if(schema.$bind) {
-        return <>{get(global, local, schema.$bind)}</>
+const renderContent = (global: any, local: any, content: any) => {
+    if (typeof content === "object") {
+        return get(global, local, content.$bind);
+    } else {
+        return content;
+    }
+}
+
+export function Renderer(props: RendererProps) {
+    const {
+        id, componentMap, childrenMap, global, local, allowedComponents
+    } = props;
+    const element = componentMap[id];
+    
+    if (element.type === "TEXT") return <>{renderContent(global, local, element.content)}</>
+
+    const sourceArrPath = element.props?.source;
+    if (element.type === '__ForEach__' && sourceArrPath) {
+        const sourceArr = get(global, local, sourceArrPath)
+        if (!Array.isArray(sourceArr)) return null;
+
+        const childrenArr = childrenMap[element.id];
+        return <>{childrenArr?.map((childId: string, index: number) => <Fragment key={index}>
+            {sourceArr.map((item: any, index1: number) => <Renderer {...props} id={childId} local={item} key={index1} />)}
+        </Fragment>)}</>
     }
 
-    if(schema.type === '__ForEach__' && schema.source && schema.template){
-        const sourceArr = get(global, local, schema.source);
-        if(!Array.isArray(sourceArr)) return null;
 
-        return <>
-            {sourceArr.map((item, index) => {
-                return <Renderer
-                    key={index}
-                    schema={schema.template!}
-                    global={global}
-                    local={item}
-                    components={components}
-                />
-            })}
-        </>
-    }
-
-    if(!schema.type) return null;
-
-    const Component = components[schema.type] || schema.type;
-    return <Component {...resolveProps(global, local, schema.props)}>
-        {schema.children?.map((item, index) => {
+    const Component = allowedComponents[element.type] || element.type;
+    return <Component {...resolveProps(global, local, element.props)}>
+        {renderContent(global, local, element.content)}
+        {childrenMap[element.id]?.map((childId: string, index: number) => {
             return <Renderer
                 key={index}
-                schema={item}
-                global={global}
-                local={local}
-                components={components}
+                {...props}
+                id={childId}
             />
         })}
     </Component>

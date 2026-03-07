f703bbb605bafa8b22d60f4b3b52ca4362d7189d
Grant <65585002+ColonelParrot@users.noreply.github.com>
2026-03-07 00:02:11 -0500

    add onError, errorFallback to GeneratedUI (#14)

--- src/templates/GeneratedUI.tsx
+++ src/templates/GeneratedUI.tsx
@@ -9,20 +9,23 @@ import { ContextfulAction, ResponseParser, SyntuxComponent, UISchema, constructI
 import spec from './spec';
 
 export interface GeneratedContentProps {
-    value: any;
-    model: LanguageModel;
-    components?: (SyntuxComponent | string)[];
-    actions?: Record<string, ContextfulAction>;
-    hint?: string;
-    placeholder?: JSX.Element;
-    cached?: string;
-    /*
-    * ^ this is a string for two reasons:
-    * - it is easier to store
-    * - it is parsed then mutated at runtime. This avoids unintended side effects.
-    */
-   onGenerate?: (arg0: string) => void;
-   skeletonize?: boolean;
+  value: any;
+  model: LanguageModel;
+  components?: (SyntuxComponent | string)[];
+  actions?: Record<string, ContextfulAction>;
+  hint?: string;
+  placeholder?: JSX.Element;
+  cached?: string;
+  /*
+  * ^ this is a string for two reasons:
+  * - it is easier to store
+  * - it is parsed then mutated at runtime. This avoids unintended side effects.
+  */
+  onGenerate?: (arg0: string) => void;
+  skeletonize?: boolean;
+
+  onError?: (arg0: any) => void;
+  errorFallback?: JSX.Element;
 }
 
 /**
@@ -36,50 +39,64 @@ export interface GeneratedContentProps {
  * @param cached Schema returned from onGenerate, used for caching UI
  * @param onGenerate Callback which accepts a string, to be passed to `cached` to reuse same UI
  * @param skeletonize Compresses value for large inputs (arrays) or untrusted input
+ * @param onError Callback which accepts an error, invoked when necessary. If not provided, runtime error occurs.
+ * @param errorFallback An element fallback to show if an error occurs during generation.
 */
 export async function GeneratedUI(props: GeneratedContentProps) {
   const input = constructInput(props);
 
-  const { value, model, components, placeholder, cached, onGenerate, actions } = props;
-  
+  const { value, model, components, placeholder, cached, onGenerate, actions, onError, errorFallback } = props;
+
   const allowedComponents = generateComponentMap(components || []);
 
   // prerender if cached
-  if(cached){
+  if (cached) {
     const parser = new ResponseParser();
     parser.addDelta(cached);
     parser.finish();
 
     const schema: UISchema = parser.schema;
 
-    if(schema.root){
+    if (schema.root) {
       return <Renderer id={schema.root.id} componentMap={schema.componentMap} childrenMap={schema.childrenMap}
-        allowedComponents={allowedComponents} global={value} local={value} actions={actions || {}}/>
+        allowedComponents={allowedComponents} global={value} local={value} actions={actions || {}} />
     } else {
       return <></>;
     }
   }
 
   const stream = createStreamableValue('');
-
   (async () => {
     let total = "";
+    let errored = false;
 
     const { textStream } = await streamText({
       model,
       system: spec,
-      prompt: input
+      prompt: input,
+      onError: (err) => {
+        stream.error(err)
+        errored = true;
+
+        if (!onError) {
+          throw err;
+        } else {
+          onError(err)
+        }
+      }
     })
 
-    for await(const delta of textStream){
+    for await (const delta of textStream) {
       stream.update(delta);
       total += delta;
     }
 
-    stream.done();
+    if (!errored) {
+      stream.done();
+    }
 
-    if(onGenerate) onGenerate(total);
+    if (onGenerate) onGenerate(total);
   })()
 
-  return <GeneratedClient value={value} allowedComponents={allowedComponents} inputStream={stream.value} placeholder={placeholder} actions={actions} />
+  return <GeneratedClient value={value} allowedComponents={allowedComponents} inputStream={stream.value} placeholder={placeholder} actions={actions} errorFallback={errorFallback} />
 }

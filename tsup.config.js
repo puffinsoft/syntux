acc08090e0503c255f7fe4e3bea46bce658397f1
Grant <65585002+ColonelParrot@users.noreply.github.com>
2026-04-20 11:51:53 -0400

    add /server to exports

--- tsup.config.js
+++ tsup.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   entry: {
     index: "src/index.ts",
     client: "src/client.ts",
+    server: "src/server/index.ts",
     "bin/cli": "src/bin/cli.mjs"
   },
   format: ["esm"],

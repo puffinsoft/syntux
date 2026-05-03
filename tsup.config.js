2b09c6e3a3fe150dff181bace495b5b1df29c9c3
Grant <65585002+ColonelParrot@users.noreply.github.com>
2026-05-03 18:26:04 -0400

    Remove cli exports (#24)

--- tsup.config.js
+++ tsup.config.js
@@ -8,8 +8,7 @@ export default defineConfig({
   entry: {
     index: "src/index.ts",
     client: "src/client.ts",
-    server: "src/server/index.ts",
-    "bin/cli": "src/bin/cli.mjs"
+    server: "src/server/index.ts"
   },
   format: ["esm"],
   dts: true,

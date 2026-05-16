7c5f61a366d65e9463fb55c7a98d92e8a353ace3
Grant <65585002+ColonelParrot@users.noreply.github.com>
2026-05-16 10:58:20 -0700

    Bump to v1.0.0, add workspaces for cli

--- tsup.config.js
+++ tsup.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
   entry: {
     index: "src/index.ts",
     client: "src/client.ts",
-    server: "src/server/index.ts"
+    server: "src/server/index.ts",
+    "bin/cli": "src/bin/cli.mjs"
   },
   format: ["esm"],
   dts: true,
@@ -31,7 +32,7 @@ export default defineConfig({
     const dest = path.resolve(__dirname, 'dist/templates');
     
     fs.cpSync(src, dest, { recursive: true });
-    
+
     console.log('[getsyntux] Templates copied to dist/templates');
   }
 });
